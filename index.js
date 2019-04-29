0950368f040e40f1488606c46c63e10862744571
JLinnell <linnelljudith@gmail.com>
2019-04-29 17:53:41 -0400

    updated css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-// const youtubeUrl = 'https://www.youtube.com/watch?v=';
+
+
 function getDataFromApi(searchTerm, callback) {
     const apiKey = 'AIzaSyDLPEE_h1mE3Mb0XcTKpkoyw0yCRIOEPGg';
     const query = {
@@ -14,12 +15,12 @@ function getDataFromApi(searchTerm, callback) {
 }
 
 function renderResult(result) {
-    console.log(result);
+    // console.log(result);
     const video_url = `https://www.youtube.com/watch?v=${result.id.videoId}/`;
     return `<div>
         <h2>
             <article class="results">
-    <a class="js-result-name" href="${video_url}" target="_blank"><span="description">${result.snippet.title}</span></a>
+    <a class="js-result-name" href="${video_url}" target="_blank"><span class="title">${result.snippet.title}</span></a>
     <a href="${video_url}" target="_blank"><img id="thumbnail" src="
     ${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}"></a><span class="description">
     ${result.snippet.description}</span></article>
