3f8f2413434c87f6794b0eb6b8c62b5986ec6fbc
JLinnell <linnelljudith@gmail.com>
2019-04-29 16:23:13 -0400

    updated parameters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 // const youtubeUrl = 'https://www.youtube.com/watch?v=';
 function getDataFromApi(searchTerm, callback) {
-    const apiKey = 'AIzaSyAai4qV-WV4BdSd-Of4zumWIlQhkmQ59WY';
+    const apiKey = 'AIzaSyDLPEE_h1mE3Mb0XcTKpkoyw0yCRIOEPGg';
     const query = {
         part: 'snippet',
         type: 'video',
