8802b7b43dcf9a99aa82793835b843fe5b602b74
JLinnell <linnelljudith@gmail.com>
2019-04-29 13:14:09 -0400

    recommit

---
+++ index.js
@@ -0,0 +1,49 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+    const query = {
+        part: 'snippet',
+        type: 'video',
+        key: 'AIzaSyCzUbhqTC_Lsoa6Gd8no837K2_FASOP_UA',
+        q: `${searchTerm} in:name`,
+        maxResults: 5
+    };
+    $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+
+}
+
+function renderResult(result) {
+    //console.log(result);
+    const video_url = `https://www.youtube.com/watch?v=${result.id.videoId}/`;
+    return `<div>
+        <h2>
+            <article class="results">
+    <a class="js-result-name" href="${video_url}" target="_blank"><span="description">${result.snippet.title}</span></a>
+    <a href="${video_url}" target="_blank"><img class="thumbnail" src="
+    ${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}"></a><span class="description">
+    ${result.snippet.description}</span></article>
+    </h2>
+    </div>`;
+
+}
+
+
+function displaySearchData(data) {
+    const results = data.items.map((item, index) => {
+        return renderResult(item)
+    });
+    $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+    $('.js-search-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        getDataFromApi(query, displaySearchData);
+    });
+}
+
+$(watchSubmit);
